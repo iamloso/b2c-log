1e674ea9cb1ab0bd2af53f533e2bac3798c7bbe1
Yunaw <291580817@qq.com>
2021-01-28 17:33:06 +0800

    update log

--- log/gormlogger.go
+++ log/gormlogger.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"fmt"
+	"go.uber.org/zap"
 	"strings"
 	"time"
 )
@@ -13,7 +14,21 @@ var layout = "2006-01-02T15:04:05.000Z0700"
 
 // Print - Log Formatter
 func (*GormLogger) Print(v ...interface{}) {
-
+	// defer修饰，会在包裹defer的函数执行完毕之后执行defer的内容
+	defer func() {
+		//recover函数将会捕获到当前的panic
+		if err := recover(); err != nil {
+			Bg().Info(
+				"sql",
+				zap.Any("src", v[1]),
+				zap.Any("duration", v[2]),
+				zap.Any("sql", v[3]),
+				zap.Any("values", v[4]),
+				zap.Any("rows_returned", v[5]),
+			)
+			return
+		}
+	}()
 	switch v[0] {
 	case "sql":
 		content := fmt.Sprintf("%v", v[3])
